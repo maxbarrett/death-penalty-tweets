998dc3cfd6eb79f2c959e79f007dabc24874e5c0
Max Barrett <maxjbarrett@gmail.com>
2015-03-16 21:03:31 +1100

    readd config file

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'p2RYrckOGPxfvFJQezpwadLbk',
+    consumer_secret: 'YjKQatqIu7CYkZNWb8Z5UNMviw3xeNffFOQUn9n7ToObh7QEim',
+    access_token_key: '119724047-ZkPXMS2O8UENHzmXuXz5gaibN61xdfL34AflME1R',
+    access_token_secret: 'a039ieRzSh7TwNusUOWu6HEteOWtQXq1H2AtYaquL7vR9'
+  }
+}
