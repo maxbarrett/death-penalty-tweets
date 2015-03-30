192aa6b6fae399a00f01bb49f924af8c6cf4cedd
Max Barrett <maxjbarrett@gmail.com>
2015-03-30 16:11:48 +1100

    readd config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// get from: https://apps.twitter.com/
+
+var twitter = {
+    consumer_key: 'p2RYrckOGPxfvFJQezpwadLbk',
+    consumer_secret: 'YjKQatqIu7CYkZNWb8Z5UNMviw3xeNffFOQUn9n7ToObh7QEim',
+    access_token_key: '119724047-ZkPXMS2O8UENHzmXuXz5gaibN61xdfL34AflME1R',
+    access_token_secret: 'a039ieRzSh7TwNusUOWu6HEteOWtQXq1H2AtYaquL7vR9'
+}
+
+exports.twitter = twitter;
