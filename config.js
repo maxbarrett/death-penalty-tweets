e1f9f5efdaebd4cc2a567f1de129a52bb70fda92
Max Barrett <maxjbarrett@gmail.com>
2015-03-30 16:01:57 +1100

    remove secret keys from repo, d'oh!

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-module.exports = {
-  twitter: {
-    consumer_key: 'p2RYrckOGPxfvFJQezpwadLbk',
-    consumer_secret: 'YjKQatqIu7CYkZNWb8Z5UNMviw3xeNffFOQUn9n7ToObh7QEim',
-    access_token_key: '119724047-ZkPXMS2O8UENHzmXuXz5gaibN61xdfL34AflME1R',
-    access_token_secret: 'a039ieRzSh7TwNusUOWu6HEteOWtQXq1H2AtYaquL7vR9'
-  }
+// get from: https://apps.twitter.com/
+
+var twitter = {
+    consumer_key: 'PUT',
+    consumer_secret: 'YOUR',
+    access_token_key: 'KEYS',
+    access_token_secret: 'HERE'
 }
+
+exports.twitter = twitter;
